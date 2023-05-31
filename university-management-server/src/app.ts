bb7a0ab3fd00018d673afbaaa47c325c4172f494
hmaminulislam <mdaminulislamsahin14@gmail.com>
2023-05-31 09:54:09 +0600

    testing

--- university-management-server/src/app.ts
+++ university-management-server/src/app.ts
@@ -1,16 +1,15 @@
-import express, { Application, json, urlencoded } from 'express';
+import express, { Application, urlencoded } from 'express'
 import cors from 'cors'
-const app:Application = express();
-const port = 3000;
+const app: Application = express()
 
 app.use(cors())
 
-// parser 
-app.use(express.json());
-app.use(urlencoded({extended: true}))
+// parser
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
