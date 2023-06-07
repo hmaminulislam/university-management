d5936c06b714c4af6baa070cdb3e733e23268278
hmaminulislam <mdaminulislamsahin14@gmail.com>
2023-06-08 01:00:38 +0600

    logger added

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./config/index";
+import {logger, errorLogger} from "./share/logger";
 
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
         app.listen(config.port, () => {
-          console.log(`University app listening on port ${config.port}`);
+          logger.info(`University app listening on port ${config.port}`)
         });
     } catch (error) {
-        console.log(`Database not connected, ${error}`)
+        errorLogger.error(`Database not connected, ${error}`)
     }
 }
 
